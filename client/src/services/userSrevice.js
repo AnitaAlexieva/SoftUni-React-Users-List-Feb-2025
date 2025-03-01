bc96ecb4ca3e08c2da0d505a2005d3238baad3a3
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-03-02 01:03:56 +0200

    Fix post data

--- client/src/services/userSrevice.js
+++ client/src/services/userSrevice.js
@@ -11,17 +11,26 @@ export default{
 
         return users
     },
-    async create(userData){
+    async create(userData) {
+        const { country, city, street, streetNumber } = userData;
+    
+        // Уверете се, че postData е дефиниран
+        const postData = { ...userData };
+    
+        postData.address = { country, city, street, streetNumber };
+        postData.createdAt = new Date().toISOString();
+        postData.updatedAt = new Date().toISOString();
+    
         const response = await fetch(baseUrl, {
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json'
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
             },
-            body:JSON.stringify(userData)
-        })
-
+            body: JSON.stringify(postData)
+        });
+    
         const result = await response.json();
-
-        return result
+        return result;
     }
+    
 }
